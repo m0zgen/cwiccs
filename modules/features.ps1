497c235a99c3873d74ffcbd8fb5591b96f08b1d0
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2020-11-26 15:30:47 +0300

    fix elevated

--- modules/features.ps1
+++ modules/features.ps1
@@ -3,13 +3,29 @@
 function checkFeatures
 {
 
-    $installedFeatures = Get-WindowsFeature -ComputerName $hostName | Where-Object { $_.Installed -match $True }
+    if ($isAdmin)
+    {
+         $installedFeatures = Get-WindowsFeature -ComputerName $hostName | Where-Object { $_.Installed -match $True }
 
-    foreach ( $feat in $installedFeatures ) {
-        regularMsg -msg "$( $feat.DisplayName )"
-        infoMsg -msg "installed - (INFO)`n"
-        bindReportArray -arrType "features" -Name "$( $feat.Path )" -state "Installed" -status "INFO"
+        foreach ( $feat in $installedFeatures ) {
+            regularMsg -msg "$( $feat.DisplayName )"
+            infoMsg -msg "installed - (INFO)`n"
+            bindReportArray -arrType "features" -Name "$( $feat.Path )" -state "Installed" -status "INFO"
+        }
     }
+    elseif ($admin -or $elevate)
+    {
+        # Can using -NoExit for debug
+        sendInfoToTerminal "You can get features list only from 'Run As Administrator' prompt"
+        bindReportArray -arrType "features" -Name "Need elevated" -state "0" -status "WARNING"
+    }
+    else
+    {
+        sendInfoToTerminal "You can get features list only from 'Run As Administrator' prompt"
+        bindReportArray -arrType "features" -Name "Need elevated" -state "0" -status "WARNING"
+    }
+
+
 } 
 
 
